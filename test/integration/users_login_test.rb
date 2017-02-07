a9ccaef8a2eaf406cf54d4647e7e0e20f1b16fa3
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-02-06 17:02:51 -0800

    add faker gem

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,6 +30,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user.id)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -39,6 +41,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+    log_in_as(@user)
+    
   end
 
   test "login with remembering" do
