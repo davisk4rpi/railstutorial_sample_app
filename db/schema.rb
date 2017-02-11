8080b5c8a05ae3b5b2edbe519c357fe7c7a47bb2
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-02-10 18:17:45 -0800

    add password reset mailer test

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20170211012943) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
-    t.string   "reset_digest"
     t.datetime "rest_sent_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
