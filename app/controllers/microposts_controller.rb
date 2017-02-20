e60258115db3db3afe33eb5114e2a16351e5fb57
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-02-19 16:17:39 -0800

    Add feed to logged_in_home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,27 @@
 class MicropostsController < ApplicationController
+	before_action :logged_in_user, only: [:create, :destroy]
+
+	def create
+		@micropost = current_user.microposts.build(micropost_params)
+  	if @micropost.save
+      flash[:success] = "You have made a post"
+      redirect_to root_url
+  	else
+  		@feed_items = []
+  		render 'static_pages/home'
+  	end
+	end
+
+	def destroy
+		'''Micropost.find(params[:id]).destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to root_url'''
+	end
+
+	private
+
+		def micropost_params
+			params.require(:micropost).permit(:content)
+  	end
+
 end
