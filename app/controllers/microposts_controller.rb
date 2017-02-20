83538c188d5418741bd49baf5aee9e07476049b8
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-02-19 16:24:55 -0800

    Users can delete posts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :correct_user,   only: :destroy
 
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
@@ -13,9 +14,9 @@ class MicropostsController < ApplicationController
 	end
 
 	def destroy
-		'''Micropost.find(params[:id]).destroy
+    @micropost.destroy
     flash[:success] = "Micropost deleted"
-    redirect_to root_url'''
+    redirect_back(fallback_location: root_url)
 	end
 
 	private
@@ -23,5 +24,10 @@ class MicropostsController < ApplicationController
 		def micropost_params
 			params.require(:micropost).permit(:content)
   	end
+
+  	def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 
 end
