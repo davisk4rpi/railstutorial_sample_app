81350436451876f993cc040be3b61303a7dfe036
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-02-19 18:42:51 -0800

    Add user microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAIIVZBELF2JLH4NOQ'],
+      :aws_secret_access_key => ENV['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']
+    }
+    config.fog_directory     =  ENV['davisk4rpi']
+  end
+end
