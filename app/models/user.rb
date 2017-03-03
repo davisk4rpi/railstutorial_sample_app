4b34bb098ab3e06a3e766d1e2c420e47f2800848
davisk4rpi <davisk4rpi@gmail.com>
2017-03-03 11:52:49 -0800

    add active_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 
 	before_save :downcase_email
   before_create :create_activation_digest
@@ -22,11 +25,11 @@ class User < ApplicationRecord
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-  
+
   def remember
   	self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end 
+  end
 
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
@@ -42,7 +45,7 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_columns(activated:    true, 
+    update_columns(activated:    true,
                    activated_at: Time.zone.now)
   end
 
@@ -73,7 +76,7 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
-  
+
   private
 
     # Converts email to all lower-case.
@@ -86,5 +89,5 @@ class User < ApplicationRecord
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
-  
+
 end
